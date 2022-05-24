c9a456cb93a1b953d0f74b9c53c4299554a4f1be
Hasyim Sri Wahyudi <hasyimsriewahyudi@gmail.com>
2022-05-24 20:21:24 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      display: ['group-focus'],
+      opacity: ['group-focus'],
+      inset: ['group-focus']
+    },
+  },
+  plugins: [],
+}
